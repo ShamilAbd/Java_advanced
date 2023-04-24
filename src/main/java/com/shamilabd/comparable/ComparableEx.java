e870110cbc1b8e668ae356f6a94e97d971b7bf30
Shamil Abdullin <abdullin@alfasystem.ru>
2023-04-24 22:25:52 +0300

    init

---
+++ src/main/java/com/shamilabd/comparable/ComparableEx.java
@@ -0,0 +1,11 @@
+package com.shamilabd.comparable;
+
+/**
+ * @author Abdullin Shamil Ildusovich
+ * @Date 24.04.2023
+ */
+public class ComparableEx {
+    public static void main(String[] args) {
+
+    }
+}
