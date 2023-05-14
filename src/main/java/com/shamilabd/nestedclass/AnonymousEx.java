ae20731c4a7df0c6cf8f4d5c7babd243e3e1bc7d
Shamil Abdullin <abdullin@alfasystem.ru>
2023-05-14 12:02:40 +0300

    Studied nested class

---
+++ src/main/java/com/shamilabd/nestedclass/AnonymousEx.java
@@ -0,0 +1,37 @@
+package com.shamilabd.nestedclass;
+
+/**
+ * @author Abdullin Shamil Ildusovich
+ * @Date 14.05.2023
+ */
+public class AnonymousEx {
+    public static void main(String[] args) {
+        MathActions actions = new MathActions() {
+            @Override
+            public int someAction(int first, int second) {
+                return first + second;
+            }
+        };
+        System.out.println(actions.someAction(5, 7));
+
+        MathActions actions2 = new MathActions() {
+            @Override
+            public int someAction(int first, int second) {
+                return first * second;
+            }
+        };
+        System.out.println(actions2.someAction(5, 7));
+
+        MathActions actions3 = new MathActions() {
+            @Override
+            public int someAction(int first, int second) {
+                return first % second;
+            }
+        };
+        System.out.println(actions3.someAction(5, 7));
+    }
+}
+
+interface MathActions {
+    int someAction(int first, int second);
+}
