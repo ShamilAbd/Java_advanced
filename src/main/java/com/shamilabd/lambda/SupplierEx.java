0bb04c8510467211f53cb67081fe24a79deb7885
Shamil Abdullin <abdullin@alfasystem.ru>
2023-05-15 21:27:24 +0300

    Studied lambdas

---
+++ src/main/java/com/shamilabd/lambda/SupplierEx.java
@@ -0,0 +1,63 @@
+package com.shamilabd.lambda;
+
+import java.util.ArrayList;
+import java.util.function.Supplier;
+/**
+ * @author Abdullin Shamil Ildusovich
+ * @Date 15.05.2023
+ */
+public class SupplierEx {
+    public static void main(String[] args) {
+        ArrayList<Car> cars = createThreeCars(() -> new Car("Nissan Tiida", "yellow", 1.6));
+        System.out.println(cars);
+    }
+
+    public static ArrayList<Car> createThreeCars(Supplier<Car> carSupplier) {
+        ArrayList<Car> cars = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            cars.add(carSupplier.get());
+        }
+        return cars;
+    }
+}
+
+class Car {
+    private String model;
+    private String color;
+    private double engine;
+
+    public Car(String model, String color, double engine) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" + model + ", " + color + ", engine=" + engine + '}';
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getEngine() {
+        return engine;
+    }
+
+    public void setEngine(double engine) {
+        this.engine = engine;
+    }
+}
