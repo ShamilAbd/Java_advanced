45f7f0f40cb0846c2c5fcd408f3df7d28ed7bb7d
Shamil Abdullin <abdullin@alfasystem.ru>
2023-06-04 20:39:01 +0300

    End RegEx

--- src/main/java/com/shamilabd/regex/RegEx.java
+++ src/main/java/com/shamilabd/regex/RegEx.java
@@ -1,5 +1,6 @@
 package com.shamilabd.regex;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import static com.shamilabd.Utils.line;
@@ -12,6 +13,14 @@ public class RegEx {
     private static int messageNum = 1;
 
     public static void main(String[] args) {
+//        patternAndMatcher();
+//        matchesAndSplit();
+//        replaceAll();
+//        groupEx();
+        printFAndFormatEx();
+    }
+
+    public static void patternAndMatcher() {
         regExTester("ABCD ABCE ABCFABCGABCH abcd abce", "ABC"); // ABC - просто совпадение с текстом
         regExTester("ABCD ABCE ABCFABCGABCH abcd abce", "[ABC]"); // [AFJ] - любой 1 из символов в скобках
         regExTester("ABDOP_ABFOP_ADCOP", "AB[C-F]OP"); // [C-F] - в диапазоне от C до F
@@ -54,5 +63,69 @@ public class RegEx {
             System.out.println("Position: " + matcher.start() + " - \"" + matcher.group() + "\"");
         }
         line();
+    }
+
+    public static void matchesAndSplit() {
+        String mail = "shamil@abdullin.ru";
+        boolean isMailAdress = mail.matches("\\w+@\\w+.(ru|com)");
+        System.out.println(isMailAdress);
+
+        mail = "shamil@abdullin.ru shamil@abdullin.ru";
+        isMailAdress = mail.matches("\\w+@\\w+.(ru|com)");
+        System.out.println(isMailAdress);
+
+        String poem = "Программный код творишь,\n"
+                + "Опутан проводами,\n"
+                + "Замученный делами,\n"
+                + "День ты и ночь сидишь.\n"
+                + "Разблокировать систему,\n"
+                + "Алгоритм какой создать —\n"
+                + "В своем деле просто гений!\n"
+                + "Он не умеет отдыхать!\n"
+                + "Комп сейчас ты отодвинь!";
+
+        String[] parts = poem.split(" ");
+        System.out.println(Arrays.toString(parts));
+
+    }
+
+    public static void replaceAll() {
+        String message = "Privet!    Kak    dela?           Ok     !";
+        System.out.println(message);
+        String newMessage = message.replaceAll(" {2,}", " ");
+        System.out.println(newMessage);
+        line();
+
+        newMessage = message.replaceFirst("\\bO\\w+", "Norm");
+        System.out.println(newMessage);
+    }
+
+    public static void groupEx() {
+        String cardFullInfo = "12345678912345670423777;65415678123345670423665;12789678456445670423789";
+        // 04/23 1234 5678 9123 4567 (789)
+        Pattern cardFormat = Pattern.compile("(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{2})(\\d{2})(\\d{3})");
+        Matcher matcher = cardFormat.matcher(cardFullInfo);
+        String newCardFormat = matcher.replaceAll("$5/$6 $1 $2 $3 $4 ($7)");
+        System.out.println(newCardFormat);
+        line();
+
+        matcher.reset();
+        while (matcher.find()) {
+            System.out.println(matcher.group(7));
+        }
+        line();
+    }
+
+    public static void printFAndFormatEx() {
+        String rawMessage = "My name is: %s, I have money: %,.2f, and I'am is a man: %b, and me %05d age.";
+        String name = "Shamil";
+        double money = 777_777.99;
+        boolean isMale = true;
+        int age = 31;
+
+        System.out.printf(rawMessage, name, money, isMale, age);
+        System.out.println();
+        String newRow = String.format(rawMessage, name, money, isMale, age);
+        System.out.println(newRow);
     }
 }
